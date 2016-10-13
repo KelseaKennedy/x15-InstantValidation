e59b597cf2c631e7452a9bc7a2165e9db098d801
Kelsea <kkennedy27@mail.csuchico.edu>
2016-10-13 00:35:38 +0000

    updated all conditions

--- form-validation.js
+++ form-validation.js
@@ -3,22 +3,24 @@ function validate(){
   var userEntered = document.getElementById("user").value;
   var passEntered = document.getElementById("pass").value;
   var sln = userEntered.length;
-  if(sln < 6) {
+  if(sln < 6 || userEntered.search(" ") != -1 ) {
   //Show message that there is an error with the username...
-  document.getElementById("usernameError").innerHTML="Bad username.";
+  document.getElementById("usernameError").innerHTML ="Bad username.";
   document.getElementById("usernameError").classList.remove("hidden-message");
   document.getElementById("usernameError").classList.add("shown-message");
   //Turn the username items red
   document.getElementById("usernameGroup").classList.add("has-error");
 }
   else
+  
 {
+  
   document.getElementById("usernameGroup").classList.add("has-success");
 }
 
-  if(passEntered == "password") {
+  if(passEntered.toLowerCase() == "password" || passEntered == userEntered || passEntered.length > 6 || passEntered.length < 20 ) {
   //Show message that there is an error with the password...
-  document.getElementById("passwordError").innerHTML="Bad password.";
+  document.getElementById("passwordError").innerHTML="Bad password."
   document.getElementById("passwordError").classList.remove("hidden-message");
   document.getElementById("passwordError").classList.add("shown-message");
   //Turn the password items red
@@ -26,6 +28,6 @@ function validate(){
 }
   else
 { 
-  document.getElementById("passwordGroup").classList.add("has-success")
+  document.getElementById("passwordGroup").classList.add("has-success");
 }
 }
